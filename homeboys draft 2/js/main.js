9f156ba287a6edcaafa3874807b776cf942f822e
homeboys-comics <67285565+homeboys-comics@users.noreply.github.com>
2020-06-23 18:51:36 +0300

    Add files via upload

---
+++ homeboys draft 2/js/main.js
@@ -0,0 +1,7 @@
+function comics_first_button() {
+    alert("Данный комикс был анонсирован. Следите за новостями в официальной группе!");
+}
+
+function comics_second_button() {
+    alert("Данный комикс находится в разработке!");
+}
